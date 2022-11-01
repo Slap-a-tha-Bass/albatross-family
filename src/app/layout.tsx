ec7f4e88a78cc55542754347629769ffc691d621
Slap-a-tha-Bass <cdeloach16@gmail.com>
2022-11-01 09:56:20 -0500

    feat: google fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,9 @@
 import '@styles/css/styles.css';
-import { Inter } from '@next/font/google';
 import Header from '@components/nav/header';
-
-const inter = Inter();
 
 export default function RootLayout({ children }: any) {
   return (
-    <html lang="en" className={inter.className}>
+    <html lang="en">
       <head>
         <title>Next.js</title>
       </head>
